7574e0e0655ebdd55f32b3afbd183e7d53eea307
Wasem1209 <philipwasem3@gmail.com>
2026-04-02 11:45:22 +0100

    Updated the sendmails

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,6 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (email, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      // Manually defining the host and port is more reliable on cloud hosts
       host: 'smtp.gmail.com',
       port: 465,
       secure: true, 
@@ -12,7 +11,11 @@ const sendEmail = async (email, subject, text) => {
         pass: process.env.EMAIL_PASS, 
       },
       
-      family: 4 
+      family: 4, 
+      
+      connectionTimeout: 10000, 
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
 
     // Send the email with the provided details
@@ -20,16 +23,16 @@ const sendEmail = async (email, subject, text) => {
       from: `"Inanst Support" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: subject,
-      html: text,
+      html: text, 
     });
     
     console.log("Email sent successfully to:", email);
   } catch (error) {
-    // Detailed logging for the Render console
     console.error("Email delivery failed details:", {
       message: error.message,
       code: error.code,
-      command: error.command
+      command: error.command,
+      response: error.response 
     });
     
     throw error; 
